07760fa13bd6fe642df58e48cff353a7303119e6
Sebastian <sebastianhothaza@gmail.com>
2022-11-17 17:54:00 -0500

    Updated home page

--- main.js
+++ main.js
@@ -1,1 +1,126 @@
-console.log("Hello from Index.js");
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/contact.js":
+/*!************************!*\
+  !*** ./src/contact.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"loadContact\": () => (/* binding */ loadContact)\n/* harmony export */ });\n\n\n\nfunction loadContact() {\n    // Select and clear the content\n    const main = document.querySelector('#main');\n    main.innerHTML = '';\n\n    // Create the page contents\n    const page = document.createElement('div');\n    page.textContent = \"contact us\"\n\n    // Attatch them\n    main.appendChild(page);\n}\n\n\n\n//# sourceURL=webpack://restaurant-page/./src/contact.js?");
+
+/***/ }),
+
+/***/ "./src/home.js":
+/*!*********************!*\
+  !*** ./src/home.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"loadHome\": () => (/* binding */ loadHome)\n/* harmony export */ });\n\n\n\nfunction loadHome() {\n    // Select and clear the content\n    const main = document.querySelector('#main');\n    main.innerHTML = '';\n\n    // Create the page contents\n    const page = document.createElement('div');\n    page.classList.add('homeBlock');\n\n    // Heading\n    const heading = document.createElement('div');\n    heading.classList.add('homeHeading');\n    heading.textContent = \"A little about our story...\"\n    page.appendChild(heading);\n\n    // Content flex-container\n    const content = document.createElement('div');\n    content.classList.add('homeContent');\n    page.appendChild(content);\n\n    // Image\n    const homeImg = document.createElement('img');\n    homeImg.setAttribute('src', 'images/chef.jpg');\n    homeImg.setAttribute('height', '250');\n    content.appendChild(homeImg);\n\n    // Description\n    const homeDesc = document.createElement('div');\n    homeDesc.textContent = \"For over 100 years, our recipes have been bringing joy & delight to families across the Greater Toronto Area. We use only the finest ingredients and time-tested techniques perfected by our masterful crafters.\";\n    content.appendChild(homeDesc);\n\n\n\n    //<img src=\"images/csbk.jpg\" alt=\"Motorcycle Racers\" height=\"300\">\n\n    // Attach them\n    main.appendChild(page);\n}\n\n\n\n//# sourceURL=webpack://restaurant-page/./src/home.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _site__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./site */ \"./src/site.js\");\n\n\n(0,_site__WEBPACK_IMPORTED_MODULE_0__.loadSite)();\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/menu.js":
+/*!*********************!*\
+  !*** ./src/menu.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"loadMenu\": () => (/* binding */ loadMenu)\n/* harmony export */ });\n\n\n\nfunction loadMenu() {\n    // Select and clear the content\n    const main = document.querySelector('#main');\n    main.innerHTML = '';\n\n    // Create the page contents\n    const page = document.createElement('div');\n    page.textContent = \"menu\"\n\n    // Attatch them\n    main.appendChild(page);\n}\n\n\n\n//# sourceURL=webpack://restaurant-page/./src/menu.js?");
+
+/***/ }),
+
+/***/ "./src/site.js":
+/*!*********************!*\
+  !*** ./src/site.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"loadSite\": () => (/* binding */ loadSite)\n/* harmony export */ });\n/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home */ \"./src/home.js\");\n/* harmony import */ var _menu__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./menu */ \"./src/menu.js\");\n/* harmony import */ var _contact__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./contact */ \"./src/contact.js\");\n\n\n\n\n\n\nfunction loadSite(){\n    const content = document.querySelector('#content');\n\n    content.appendChild(createHeader());\n    content.appendChild(createNav());\n    content.appendChild(createMain());\n\n    (0,_home__WEBPACK_IMPORTED_MODULE_0__.loadHome)();\n}\n\nfunction createHeader(){\n    const header = document.createElement('div');\n    header.classList.add('header');\n    header.textContent = \"Sibiu Sweets\";\n    return header;\n}\n\nfunction createNav(){\n    // Creation of the parent container\n    const nav = document.createElement('div');\n    nav.classList.add('navBar');\n\n    // Creation of the individual elements\n    const navHome = document.createElement('button');\n    const navMenu = document.createElement('button');\n    const navContact = document.createElement('button');\n\n    navHome.classList.add('navBtn');\n    navMenu.classList.add('navBtn');\n    navContact.classList.add('navBtn');\n\n    navHome.textContent = \"Home\";\n    navMenu.textContent = \"Menu\";\n    navContact.textContent = \"Contact Us\";\n\n    // Adding event listeners\n    navHome.addEventListener('click', () => {\n        if (navHome.classList.contains('active')){\n            return;\n        } else {\n            setActiveBtn(navHome);\n            (0,_home__WEBPACK_IMPORTED_MODULE_0__.loadHome)();\n        }\n    });\n    navMenu.addEventListener('click', () => {\n        if (navMenu.classList.contains('active')){\n            return;\n        } else {\n            setActiveBtn(navMenu);\n            (0,_menu__WEBPACK_IMPORTED_MODULE_1__.loadMenu)();\n        }\n    });\n    navContact.addEventListener('click', () => {\n        if (navContact.classList.contains('active')){\n            return;\n        } else {\n            setActiveBtn(navContact);\n            (0,_contact__WEBPACK_IMPORTED_MODULE_2__.loadContact)();\n        }\n    });\n\n\n    // Adding individual elements to parent container\n    nav.appendChild(navHome);\n    nav.appendChild(navMenu);\n    nav.appendChild(navContact);\n\n    // We set the active button to home on default\n    setActiveBtn(navHome);\n\n    return nav;\n}\n\nfunction createMain(){\n    const main = document.createElement('div');\n    main.classList.add(\"main\");\n    main.setAttribute('id','main');\n    return main;\n}\n\n// Given a button in nav bar, sets it to active\nfunction setActiveBtn(btn){\n    const navBtns = document.querySelectorAll('.navBtn');\n\n    // We remove active status of all buttons first\n    navBtns.forEach((button) => {\n        button.classList.remove('active');\n    });\n\n    // Now, give the corresponding button the correct trait\n    btn.classList.add('active');\n}\n\n//# sourceURL=webpack://restaurant-page/./src/site.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
